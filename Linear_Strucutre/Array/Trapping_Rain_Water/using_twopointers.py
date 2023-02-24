9abd588ff8a810c784adb8431c75e5425eaa46b6
JIY00N2 <angella990825@gmail.com>
2023-02-24 16:35:32 +0900

    Trapping Rain Water

---
+++ Linear_Strucutre/Array/Trapping_Rain_Water/using_twopointers.py
@@ -0,0 +1,27 @@
+# O(n)
+from typing import List
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        if not height:
+            return 0
+
+        volume = 0
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right] # left_max=height[left]=0, right_max=height[right]=1
+
+        while left < right:
+            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+            # 더 높은 쪽을 향해 투 포인터 이동
+            if left_max <= right_max:
+                volume += left_max - height[left]
+                left += 1
+            else:
+                volume += right_max - height[right]
+                right -= 1
+        return volume
+
+solution = Solution()
+height:  list = [0,1,0,2,1,0,1,3,2,1,2,1]
+result: int = solution.trap(height)
+print(result)
